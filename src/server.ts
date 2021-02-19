58a449cd5171b9ef16f1e8747e99e4eeefaa206e
William <william48238@gmail.com>
2021-02-19 19:13:38 -0300

    trying to fix again 2

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import express from 'express';
 import routes from './rotes'
 
 const app = express();
+const PORT : string|number = process.env.PORT || 5000;
 
 app.use(routes);
 app.use(express.json());
 
-app.listen(3000 || process.env.PORT);
+app.listen(PORT,() => console.log(`hosting @${PORT}`));
