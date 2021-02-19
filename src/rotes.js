f58a2a9801205ced732b7fae8ab5e73e8911f2ce
William <william48238@gmail.com>
2021-02-19 19:04:52 -0300

    trying to fix again 2

--- src/rotes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var routes = express_1.Router();
-routes.get('/:message', function (req, res) {
-    var message = req.params.message;
-    return res.send(message);
-});
-exports.default = routes;
