7bc2b3ae8201904b9f1ea3147d2d6847a997767e
William <william48238@gmail.com>
2021-03-04 15:27:59 -0300

    adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { resolve } from 'path'
+import cors from 'cors';
+import { resolve } from 'path';
 
 import routes from './routes';
 
@@ -8,8 +9,9 @@ import './database/connection';
 const app = express();
 const PORT : string|number = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
-app.use('/images', express.static(resolve(__dirname, 'tmp', 'uploads')))
+app.use('/images', express.static(resolve(__dirname, 'tmp', 'uploads')));
 app.use(routes);
 
 app.listen(PORT, () => console.log(`hosting @${PORT}`));
